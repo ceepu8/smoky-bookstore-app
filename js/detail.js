e18349a1538c1382fe903428798c19d8d1c3ceaa
ceepu8 <caophuonguyen132002@gmail.com>
2023-04-23 09:44:47 +0700

    remove tailwind in detail page

--- js/detail.js
+++ js/detail.js
@@ -46,32 +46,28 @@ function renderBook(book) {
         </ul>
       </div>
       <div class="book-details">
-        <span class="rounded-full py-2 px-4 bg-[#f44244] text-white max-w-fit text-xs leading-1">${
-          book.status
-        }</span>
-        <h1 class="text-3xl font-medium">${book.name}</h1>
-        <h2 class="text-2xl">Author: ${book.author}</h2>
-        <p class="text-2xl text-red-500">$${book.price.toFixed(2)}</p>
+        <span class="status-badge">${book.status}</span>
+        <h1 class="book-name">${book.name}</h1>
+        <h2 class="book-author">Author: ${book.author}</h2>
+        <p class="book-price">$${book.price.toFixed(2)}</p>
         <p>Genre: ${book.genre.join(", ")}</p>
         <p>Description: ${book.description}</p>
         <p>Released Date: ${book.released_date}</p>
 
-        <div class="flex gap-x-4 items-center border border-solid border-gray-200 max-w-fit px-4 py-2 rounded-full text-gray-600">
+        <div class="quantity-btns">
           <button><i class="fa-solid fa-plus"></i></button>
           <span>1</span>
           <button><i class="fa-solid fa-minus"></i></button>
         </div>
 
-        <div>
-          <button class="uppercase px-6 py-3 bg-[#f44244] text-white font-medium ">Add to cart</button>
-        </div> 
+        <button class="primary-button size-lg max-w-fit">Add to cart</button>
 
 
-        <div class="flex items-center gap-x-4">
+        <div class="sharing-social-media">
           <span>Share:</span>
-          <div class="w-8 h-8 flex items-center rounded text-white justify-center bg-blue-600"><i class="fa-brands text-lg fa-facebook-f"></i></div>
-          <div class="w-8 h-8 flex items-center rounded text-white justify-center bg-blue-400"><i class="fa-brands text-lg fa-twitter"></i></div>
-          <div class="w-8 h-8 flex items-center rounded text-white justify-center bg-pink-500"><i class="fa-brands text-lg fa-instagram"></i></div>
+          <div class="icon-wrapper facebook"><i class="fa-brands fa-facebook-f"></i></div>
+          <div class="icon-wrapper twitter"><i class="fa-brands fa-twitter"></i></div>
+          <div class="icon-wrapper instagram"><i class="fa-brands fa-instagram"></i></div>
         </div>
       </div>
     </div>
